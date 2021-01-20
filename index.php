5ff0cb396f1355e17fc7268acfa5fd3a59436b05
Sergey Kirgizov <cepxuo1@ya.ru>
2021-01-21 00:13:11 +0500

    fix instance meta-data url

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <center>
   <h2><font color = "gold">Test Web Server was built using Terraform <font color = "aqua"> v0.14</font></h2><br><br>
   <font color = "red">This page is stored in <b><a href="https://github.com/cepxuo/webpage">GitHub</a></b><font><br><br>
-  <font colog = "gold">Server internal IP: <?php echo @file_get_contents("http://instance-data/latest/meta-data/local-ipv4"); ?></font><br><br>
+  <font colog = "gold">Server internal IP: <?php echo @file_get_contents("http://169.254.169.254/instance-data/latest/meta-data/local-ipv4"); ?></font><br><br>
 </center>
 
 <font color = "green">
